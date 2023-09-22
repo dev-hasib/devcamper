4d9154e26d954406736a1e14908780f304cbe5aa
AL HASIB <hasib2653@gmail.com>
2023-09-23 00:38:53 +0600

    add mongodb through mongoose

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connectionDB = async () => {
+	const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+	});
+	console.log('DB connected on: ', conn.connection.host);
+};
+
+module.exports = connectionDB;
--- server.js
+++ server.js
@@ -3,17 +3,13 @@ const express = require('express');
 
 // local imports from local files
 const bootCamp = require('./router/bootcamp');
+const connectionDB = require('./db');
+const logger = require('./middleware/logger');
 
 const app = express();
 
+app.use(logger);
 app.use('/api/v1/bootcamp/', bootCamp);
-
-app.get('/', (req, res) => {
-	res.status(200).json({
-		success: true,
-		msg: 'This is the root route of this application!',
-	});
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
@@ -21,3 +17,5 @@ app.listen(PORT, () => {
 		`Server is running ${process.env.NODE_ENV} mode on port: ${PORT}`
 	);
 });
+
+connectionDB();
